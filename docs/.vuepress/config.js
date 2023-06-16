39e1cdf90ed2bb0d36908c534ccb01e71cfef82b
高博宇 <gaoboyu@189.cn>
2023-06-16 20:12:38 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    title: 'TypeScript4 文档',
+    description: 'TypeScript4 最新官方文档翻译',
+    theme: 'reco',
+    base: '/',
+    themeConfig: {
+      nav: [
+          { text: '首页', link: '/' },
+          { 
+              text: '冴羽的 JavaScript 博客', 
+              items: [
+                  { text: 'Github', link: 'https://github.com/mqyqingfeng' },
+                  { text: '掘金', link: 'https://juejin.cn/user/712139234359182/posts' }
+                ]
+            }
+        ]
+    }
+
+  }
