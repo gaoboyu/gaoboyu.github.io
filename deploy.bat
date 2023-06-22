859515317dbadae0b68bac32ebd7e88389f38f8e
高博宇 <gaoboyu@189.cn>
2023-06-22 09:27:09 +0800

    'deploy'

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,15 @@
-cd C:\Users\gaobo\OneDrive\Vuepress\.vuepress\dist
-git init
-git add -A
+copy C:\Users\gaobo\OneDrive\Vuepress\.vuepress\dist D:\Space\gaoboyu.github.io
+
+copy C:\Users\gaobo\OneDrive\Vuepress\.vuepress\dist\assets D:\Space\Assets
+
+cd D:\Space\gaoboyu.github.io
+
 git commit -m 'deploy'
-git push -f git@gitee.com:gaoboyu/gaoboyu.git master:gh-pages
+git add .
+git push -u origin master
+
+D:\Space\Assets
+
+git commit -m 'deploy'
+git add .
+git push -u origin master
